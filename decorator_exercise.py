6ce7f402c5470557371c488b0cb7075047b12d6c
André <84719490+gggandre@users.noreply.github.com>
2023-04-18 17:40:14 -0600

    a

--- decorator_exercise.py
+++ decorator_exercise.py
@@ -3,37 +3,41 @@ from typing import Callable
 
 def star(func: Callable) -> Callable:
     """A decorator that prints 15 * before and after the function call"""
-    # TODO(students): implement this function
-    pass
+    def wrapper(*args, **kwargs):
+        print("*" * 15)
+        func(*args, **kwargs)
+        print("*" * 15)
+    return wrapper
 
 
 def reverse_first_arg_string(func: Callable) -> Callable:
     """A decorator that reverses the first argument of the function call"""
-    # TODO(students): implement this function
-    pass
+    def wrapper(*args, **kwargs):
+        args = list(args)
+        if len(args) > 0 and isinstance(args[0], str):
+            args[0] = args[0][::-1]
+        func(*args, **kwargs)
+    return wrapper
 
 
 @star
 @reverse_first_arg_string
-def printer(msg1, msg2, msg3):
-    print(msg1)
-    print(msg2)
-    print(msg3)
+def printer(msg):
+    print(msg)
 
 
-if __name__ == "__main__":
-    printer("i love python", "1234", "abcd")
+printer("i love python")
+
+
 """
 Desired Behavior:
 
 INPUT:
-printer("i love python", "1234", "abcd")
+printer("i love python")
 
 OUTPUT:
 ***************
 nohtyp evol i
-1234
-abcd
 ***************
 
 """
